b9ae527855ee7cc5f1902a388eb5ce2ad03d0bf8
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-11-29 10:11:56 +0100

    Update Loading.jsx

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -3,7 +3,7 @@ import { LoadingAnimation } from "../assets/LoadingAnimation";
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center w-screen h-screen gap-6 overflow-hidden bg-black/80">
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center w-screen h-screen space-y-4 overflow-hidden bg-black/80">
       <LoadingAnimation style="mr-2 h-20 w-20 animate-spin fill-white text-gray-600" />
       <h2 className="text-2xl font-semibold text-center text-white">
         Loading...
